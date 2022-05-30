c1bb739d6d5924a1773bc9019e92c5169e30b252
MateoAse <1807238@ucc.edu.ar>
2022-05-30 15:43:03 -0300

    se agrego documentacion

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,13 +1,13 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
+ * conversion de unidades, de metros, yardas, pies y pulgadas.
+ * @method cambiarunidades
+ * @param {string} id - el id de los imputs de metros,yardas, pies y pulgadas.
+ * @param {nombre} valor - el valor de los imputs de metros, yardas, pies o pulgadas
+ * @return
  */
 function cambiarunidades(id, valor) {
     if (isNaN(valor)){
-        alert("se ingreso un valor invalido");
+        alert("se ingreso un valor invalido"+id);
         document. lasunidades.unid_metro.value = "";
         document.lasunidades.unid_pulgada.value = "";
         document.lasunidades.unid_pie.value = "";
@@ -17,5 +17,17 @@ function cambiarunidades(id, valor) {
         document.lasunidades.unid_pie.value = 3.28084*valor;
         document.lasunidades.unid_yarda.value = 1.09361*valor;
 
+    }else if(id=="pulgada"){
+        document.lasunidades.unid_metro.value = 0.0254*valor;
+        document.lasunidades.unid_pie.value = 0.833333*valor;
+        document.lasunidades.unid_yarda.value = 0.0277778*valor;
+    }else if(id=="pie"){
+        document.lasunidades.unid_metro.value = 0.3048*valor;
+        document.lasunidades.unid_pulgada.value = 12*valor;
+        document.lasunidades.unid_yarda.value = 0.333333*valor;
+    }else if(id=="yarda"){
+        document.lasunidades.unid_metro.value = 0.9144*valor;
+        document.lasunidades.unid_pulgada.value = 36*valor;
+        document.lasunidades.unid_pie.value = 3*valor;
     }
 }
