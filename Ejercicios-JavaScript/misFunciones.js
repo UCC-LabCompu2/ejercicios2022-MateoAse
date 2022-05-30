c130ab8dd0d507c79d24a16d8232b6f3c2b714d8
MateoAse <1807238@ucc.edu.ar>
2022-05-30 14:52:58 -0300

    ejercicio de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,9 +5,17 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
-function cambiarunidades(id. valor) {
+function cambiarunidades(id, valor) {
     if (isNaN(valor)){
         alert("se ingreso un valor invalido");
         document. lasunidades.unid_metro.value = "";
+        document.lasunidades.unid_pulgada.value = "";
+        document.lasunidades.unid_pie.value = "";
+        document.lasunidades.unid_yarda.value = "";
+    }else if(id=="Metro"){
+        document.lasunidades.unid_pulgada.value = 39.3701*valor;
+        document.lasunidades.unid_pie.value = 3.28084*valor;
+        document.lasunidades.unid_yarda.value = 1.09361*valor;
+
     }
 }
