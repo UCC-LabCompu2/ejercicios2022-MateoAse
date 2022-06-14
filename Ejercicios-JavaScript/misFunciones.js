8d520ec641f86752939e0ad98dae34cb3f9e8414
MateoAse <1807238@ucc.edu.ar>
2022-06-14 19:15:49 -0300

    ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,29 +5,29 @@
  * @param {nombre} valor - el valor de los imputs de metros, yardas, pies o pulgadas
  * @return
  */
-function cambiarunidades(id, valor) {
-    if (isNaN(valor)){
-        alert("se ingreso un valor invalido"+id);
+function cambiarunidades(id, value) {
+    if (isNaN(value)){
+        alert("se ingreso un valor invalido "+id);
         document. lasunidades.unid_metro.value = "";
         document.lasunidades.unid_pulgada.value = "";
         document.lasunidades.unid_pie.value = "";
         document.lasunidades.unid_yarda.value = "";
     }else if(id=="Metro"){
-        document.lasunidades.unid_pulgada.value = 39.3701*valor;
-        document.lasunidades.unid_pie.value = 3.28084*valor;
-        document.lasunidades.unid_yarda.value = 1.09361*valor;
+        document.lasunidades.unid_pulgada.value = 39.3701*value;
+        document.lasunidades.unid_pie.value = 3.28084*value;
+        document.lasunidades.unid_yarda.value = 1.09361*value;
 
     }else if(id=="pulgada"){
-        document.lasunidades.unid_metro.value = 0.0254*valor;
-        document.lasunidades.unid_pie.value = 0.833333*valor;
-        document.lasunidades.unid_yarda.value = 0.0277778*valor;
+        document.lasunidades.unid_metro.value = 0.0254*value;
+        document.lasunidades.unid_pie.value = 0.833333*value;
+        document.lasunidades.unid_yarda.value = 0.0277778*value;
     }else if(id=="pie"){
-        document.lasunidades.unid_metro.value = 0.3048*valor;
-        document.lasunidades.unid_pulgada.value = 12*valor;
-        document.lasunidades.unid_yarda.value = 0.333333*valor;
+        document.lasunidades.unid_metro.value = 0.3048*value;
+        document.lasunidades.unid_pulgada.value = 12*value;
+        document.lasunidades.unid_yarda.value = 0.333333*value;
     }else if(id=="yarda"){
-        document.lasunidades.unid_metro.value = 0.9144*valor;
-        document.lasunidades.unid_pulgada.value = 36*valor;
-        document.lasunidades.unid_pie.value = 3*valor;
+        document.lasunidades.unid_metro.value = 0.9144*value;
+        document.lasunidades.unid_pulgada.value = 36*value;
+        document.lasunidades.unid_pie.value = 3*value;
     }
 }
